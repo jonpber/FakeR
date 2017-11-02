b433723a25b1ed0655c507524b4e924560154624
Jon Berry <jonpber@gmail.com>
2017-11-01 20:27:42 -0700

    added scrollTo, contact now works, fixed overflow issue on main page, lowered video size for web

--- assets/js/site.js
+++ assets/js/site.js
@@ -8,5 +8,24 @@ $(function(){
     $(window).resize(function(){
 	    $('.bannerGallery').css('height', $(window).height());
 	});
+
+	$(".nav-link").on("click", function(){
+		$('.navbar-collapse').collapse('hide');
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top -70
+		}, 800, function(){
+		})
+	})
+
+	$("form").on("submit", function(event){
+		event.preventDefault();
+		$("input").empty();
+		$.toast({
+						heading: 'Message Sent!',
+						text : "Thanks for getting in touch!",
+						loader: false,
+						showHideTransition: 'slide'
+					})
+	})
 })
 
